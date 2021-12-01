079a24f46da238cce1bd062284de78b56f6dc4f6
Burak Yiğit Uslu <burakyuslu@gmail.com>
2021-12-01 17:24:05 +0300

    Added frontend_readme

---
+++ project/frontend/frontend_readme.txt
@@ -0,0 +1,4 @@
+npm run electron => Runs the app on electron window only.
+npm run dev => Runs the app as a web-app as well as on the electron window.
+
+Other npm run options will need to be configured before deployment.
--- project/frontend/prexcel/src/App.js
+++ project/frontend/prexcel/src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Text text text lorem ipsum deneme deneme.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here
         </a>
       </header>
     </div>
