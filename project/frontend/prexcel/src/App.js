5d6716feda356e8aec77213ba96771370b114f2a
Burak Yiğit Uslu <burakyuslu@gmail.com>
2021-12-02 16:34:52 +0300

    Added electron.

--- project/frontend/prexcel/src/App.js
+++ project/frontend/prexcel/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Text text text
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hyperlink hyperlink hyperlink
         </a>
       </header>
     </div>
