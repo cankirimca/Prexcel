d8e85c42bf5a357210ddd09fd40706465fe4ac95
Burak Yiğit Uslu <burakyuslu@gmail.com>
2021-12-21 12:41:02 +0300

    Updated dependencies, modified the package.json % scripts.

--- project/frontend/prexcel/src/App.js
+++ project/frontend/prexcel/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hyperlink hyperlink hyperlink
+          hyperlink hyperlink hyperlink mahmut
         </a>
       </header>
     </div>
