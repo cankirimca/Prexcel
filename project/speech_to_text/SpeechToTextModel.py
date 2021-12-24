316f12170fdce4b36da689f88bf3e92dc48329b4
Can Kırımca <can.kirimca@ug.bilkent.edu.tr>
2021-12-25 02:50:14 +0300

    added live transcription functionality

--- project/speech_to_text/SpeechToTextModel.py
+++ project/speech_to_text/SpeechToTextModel.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import wave
 
+from Prexcel.project.speech_to_text.VoiceDetectionManager import VoiceDetectionManager
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 pbmm_path = "deepspeech-0.9.3-models.pbmm"
@@ -11,14 +13,18 @@ scorer_path ="deepspeech-0.9.3-models.scorer"
 beam_width = 1000
 lm_alpha = 0.93
 lm_beta = 1.18
+DEFAULT_SAMPLE_RATE = 16000
 
 class SpeechToTextModel:
+
     def __init__(self):
         self.model = Model(pbmm_path)
         self.model.enableExternalScorer(scorer_path)
         self.model.setScorerAlphaBeta(lm_alpha, lm_beta)
         self.model.setBeamWidth(beam_width)
         self.stream = self.model.createStream()
+        self.vdm = VoiceDetectionManager()
+
 
     def read_wav_file(self, filename):
         wf = wave.open(filename, 'rb')
@@ -46,10 +52,17 @@ class SpeechToTextModel:
             print(self.stream.intermediateDecode())
             start = end
 
-    def transcribe_live(self, speech_data):
-        data16 = np.frombuffer(speech_data, dtype=np.int16)
-        return self.model.stt(data16)
+    def transcribe_live(self):
+        frames = self.vdm.vad_collector()
+        for frame in frames:
+            if frame is not None:
+                # if spinner:
+                #     spinner.start()
+                self.stream.feedAudioContent(np.frombuffer(frame, np.int16))
+                print(self.stream.intermediateDecode())
+
+
 
 stm = SpeechToTextModel()
-x = stm.transcribe_stream("speech2.wav")
+x = stm.transcribe_live()
 #print(x)
---
+++ project/speech_to_text/VoiceDetectionManager.py
@@ -0,0 +1,101 @@
+import collections
+import logging
+import queue
+
+from deepspeech import Model
+import deepspeech
+import webrtcvad
+import numpy as np
+import os
+import wave
+import pyaudio
+import time
+
+from halo import Halo
+from scipy import signal
+
+pbmm_path = "deepspeech-0.9.3-models.pbmm"
+scorer_path ="deepspeech-0.9.3-models.scorer"
+beam_width = 1000
+lm_alpha = 0.93
+lm_beta = 1.18
+input_rate = 16000
+
+class VoiceDetectionManager(object):
+
+    RATE_PROCESS = 16000
+    CHANNELS = 1
+    BLOCKS_PER_SECOND = 50
+    input_rate = 16000
+
+    def __init__(self):
+        def callback(data):
+            self.queue.put(data)
+
+        def proxy_callback(in_data, frame_count, time_info, status):
+            if self.chunk is not None:
+                in_data = self.wf.readframes(self.chunk)
+            callback(in_data)
+            return None, pyaudio.paContinue
+
+        self.block_size = int(self.RATE_PROCESS / float(self.BLOCKS_PER_SECOND))
+        self.queue = queue.Queue()
+        self.sample_rate = self.RATE_PROCESS
+        self.block_size_input = int(self.input_rate / float(self.BLOCKS_PER_SECOND))
+        self.input_rate = input_rate
+        self.vad = webrtcvad.Vad(3)
+        self.pAudio = pyaudio.PyAudio()
+
+        kwargs = {
+            'format': pyaudio.paInt16,
+            'channels': self.CHANNELS,
+            'rate': self.input_rate,
+            'input': True,
+            'frames_per_buffer': self.block_size_input,
+            'stream_callback': proxy_callback,
+        }
+
+        self.chunk = None
+        self.stream = self.pAudio.open(**kwargs)
+        self.stream.start_stream()
+
+    def read(self):
+        return self.queue.get()
+
+    frame_duration_ms = property(lambda self: 1000 * self.block_size // self.sample_rate)
+
+    def frame_generator(self):
+        if self.input_rate == self.RATE_PROCESS:
+            while True:
+                yield self.read()
+
+    def vad_collector(self):
+        frames = self.frame_generator()
+        ring_buffer = collections.deque(maxlen=300 // self.frame_duration_ms)
+        triggered = False
+
+        for frame in frames:
+            if len(frame) < 640:
+                return
+
+            is_speech = self.vad.is_speech(frame, self.sample_rate)
+
+            if not triggered:
+                ring_buffer.append((frame, is_speech))
+                num_voiced = len([f for f, speech in ring_buffer if speech])
+                if num_voiced > 0.75 * ring_buffer.maxlen:
+                    triggered = True
+                    for f, s in ring_buffer:
+                        yield f
+                    ring_buffer.clear()
+
+            else:
+                yield frame
+                ring_buffer.append((frame, is_speech))
+                num_unvoiced = len([f for f, speech in ring_buffer if not speech])
+                if num_unvoiced > 0.75 * ring_buffer.maxlen:
+                    triggered = False
+                    yield None
+                    ring_buffer.clear()
+
+print("Run VoiceDetectionManager")
