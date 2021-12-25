0001db126bdd18711c5ff7004d5f2e2b101da0cf
Burak Yiğit Uslu <burakyuslu@gmail.com>
2021-12-25 13:56:15 +0300

    Fixed main menu.

--- project/frontend/prexcel/src/pages/MainMenu.js
+++ project/frontend/prexcel/src/pages/MainMenu.js
@@ -3,6 +3,9 @@ import {render} from "react-dom";
 import * as ReactDOM from "react-dom";
 import ScreenIds from "./ScreenIds";
 
+import {Button, Grid, Paper, TextField} from "@mui/material";
+
+
 const MainMenu = (props) => {
 
   function logOut() {
@@ -15,15 +18,37 @@ const MainMenu = (props) => {
     props.onMainMenuHandler(ScreenIds.MY_PRESENTATIONS_SCREEN_ID);
   }
 
+   function doNothing() {
+      props.onMainMenuHandler(ScreenIds.MAIN_MENU_SCREEN_ID);
+   }
+
     return (
         <div>
-            <p>
-                main menu page
-            </p>
+           <Grid container spacing={2}>
 
-          <button onClick={goToMyPresentations}> My Presentations</button><br/>
+              <Grid item xs={12}>
+                 <h1 style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}} >
+                    Prexcel
+                 </h1><br/>
+                 <h1 style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}} >
+                    The Multifunctional Presentation Assistant
+                 </h1><br/>
+              </Grid>
 
-          <button onClick={logOut}>Log Out</button><br/><br/>
+              <Grid item xs={4}/>
+              <Grid item xs={4}>
+                 <Paper style={{marginTop: '25%', marginBottom:'5%', flexDirection:'row', alignItems:'center', justifyContent:'center'}} elevation={3}>
+
+                       <Button style={{ marginTop: '5%', marginBottom: '5%'}} variant="contained" onClick={goToMyPresentations}>My Presentations</Button> <br/>
+                       <Button style={{ marginBottom: '5%'}} variant="contained" onClick={doNothing}>Tutorial of Prexcel</Button> <br/>
+                       <Button style={{ marginBottom: '5%'}} variant="contained" onClick={doNothing}>Tutorial of Presentations</Button> <br/>
+                       <Button style={{ marginBottom: '5%'}} variant="contained" onClick={doNothing}>User-Account-Details</Button> <br/>
+                       <Button style={{ marginBottom: '5%'}} variant="contained" onClick={logOut}>Log-Out</Button>
+
+                 </Paper>
+              </Grid>
+              <Grid item xs={4}/>
+           </Grid>
         </div>
 
     );
