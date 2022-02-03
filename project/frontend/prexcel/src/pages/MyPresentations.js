85045d7605845d9571d1dd5bac7ce4b53df762ef
BarisTiftik <brstftk@gmail.com>
2022-02-03 21:43:34 +0300

    Indentation

--- project/frontend/prexcel/src/pages/MyPresentations.js
+++ project/frontend/prexcel/src/pages/MyPresentations.js
@@ -6,31 +6,31 @@ import {Button, Grid, Paper, TextField} from "@mui/material";
 
 // todo to be removed once connected to the database
 const columns = [
-  {
-    field: 'id',
-    headerName: 'ID',
-    width: 90
-  },
-  {
-    field: 'name',
-    headerName: 'Name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'transcript',
-    headerName: 'Transcript',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'user_id',
-    headerName: 'UserID',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
+   {
+      field: 'id',
+      headerName: 'ID',
+      width: 90
+   },
+   {
+      field: 'name',
+      headerName: 'Name',
+      width: 150,
+      editable: true,
+   },
+   {
+      field: 'transcript',
+      headerName: 'Transcript',
+      type: 'number',
+      width: 110,
+      editable: true,
+   },
+   {
+      field: 'user_id',
+      headerName: 'UserID',
+      type: 'number',
+      width: 110,
+      editable: true,
+   },
 ];
 
 // todo to be removed once connected to the database
@@ -45,86 +45,86 @@ const columns = [
 
 export default function MyPresentations(props) {
 
-  useEffect(fetchPresentations, []);
+   useEffect(fetchPresentations, []);
 
-  const [rows, setRows] = useState([]);
+   const [rows, setRows] = useState([]);
 
-  function goToPresentationDetails() {
-    props.onMyPresentationsHandler(ScreenIds.PRESENTATION_DETAILS_SCREEN_ID);
-  }
+   function goToPresentationDetails() {
+      props.onMyPresentationsHandler(ScreenIds.PRESENTATION_DETAILS_SCREEN_ID);
+   }
 
-  const [selectionModel, setSelectionModel] = useState(() => rows.filter((r) => true).map((r) => r.presentation_id));
+   const [selectionModel, setSelectionModel] = useState(() => rows.filter((r) => true).map((r) => r.presentation_id));
 
-  function goBackToMainMenu() {
-    props.onMyPresentationsHandler(ScreenIds.MAIN_MENU_SCREEN_ID);
-  }
+   function goBackToMainMenu() {
+      props.onMyPresentationsHandler(ScreenIds.MAIN_MENU_SCREEN_ID);
+   }
 
-  function fetchPresentations() {
-    getPresentations(userData);
-  }
+   function fetchPresentations() {
+      getPresentations(userData);
+   }
 
-  const getPresentations = (userID) => {
-    let success = true;
-    fetch('http://localhost:5000/getPresentations', {
-      method: 'POST',
-      headers: {
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(userID)
-    })
-       .then((resp) => {
-         return resp.json()
-       })
-       .then((data) => {
-         console.log(data);
-         setRows(fixData(data));
-         console.log(rows);
-       })
-       .catch(error => console.log(error))
+   const getPresentations = (userID) => {
+      let success = true;
+      fetch('http://localhost:5000/getPresentations', {
+         method: 'POST',
+         headers: {
+            'Content-Type':'application/json'
+         },
+         body:JSON.stringify(userID)
+      })
+         .then((resp) => {
+            return resp.json()
+         })
+         .then((data) => {
+            console.log(data);
+            setRows(fixData(data));
+            console.log(rows);
+         })
+         .catch(error => console.log(error))
 
-    return success;
-  };
+      return success;
+   };
 
-  const userData = {
-    "userID": props.currentUserID
-  };
+   const userData = {
+      "userID": props.currentUserID
+   };
 
-  function fixData (arr2d) {
+   function fixData (arr2d) {
 
-    let fixedRows = [];
+      let fixedRows = [];
 
-    for (let i = 0; i < arr2d.length; i++) {
-      let newObj = {};
-      newObj.id = arr2d[i][0];
-      newObj.name = arr2d[i][1];
-      newObj.transcript = arr2d[i][2];
-      newObj.user_id = arr2d[i][3];
-      fixedRows.push(newObj);
-    }
-    return fixedRows;
-  }
+      for (let i = 0; i < arr2d.length; i++) {
+         let newObj = {};
+         newObj.id = arr2d[i][0];
+         newObj.name = arr2d[i][1];
+         newObj.transcript = arr2d[i][2];
+         newObj.user_id = arr2d[i][3];
+         fixedRows.push(newObj);
+      }
+      return fixedRows;
+   }
 
-  return (
-     <div>
-       <div>
-         {fetchPresentations}
-       </div>
-    <div style={{ height: 400, width: '100%' }}>
-      <p>My Presentations</p>
-      <DataGrid
-         rows={rows}
-         columns={columns}
-         pageSize={5}
-         rowsPerPageOptions={[5]}
-         checkboxSelection
-         disableSelectionOnClick
+   return (
+      <div>
+         <div>
+            {fetchPresentations}
+         </div>
+         <div style={{ height: 400, width: '100%' }}>
+            <p>My Presentations</p>
+            <DataGrid
+               rows={rows}
+               columns={columns}
+               pageSize={5}
+               rowsPerPageOptions={[5]}
+               checkboxSelection
+               disableSelectionOnClick
 
-         selectionModel={selectionModel}
-         onSelectionModelChange={setSelectionModel}
-      />
+               selectionModel={selectionModel}
+               onSelectionModelChange={setSelectionModel}
+            />
 
-      <br/><Button variant="contained" onClick={goToPresentationDetails}>Presentation Details</Button><br/><br/>
-      <Button variant="contained" onClick={goBackToMainMenu}>Back To Main Menu</Button>
-    </div></div>
-  );
+            <br/><Button variant="contained" onClick={goToPresentationDetails}>Presentation Details</Button><br/><br/>
+            <Button variant="contained" onClick={goBackToMainMenu}>Back To Main Menu</Button>
+         </div></div>
+   );
 }
--- project/frontend/prexcel/src/pages/PresentationDetails.js
+++ project/frontend/prexcel/src/pages/PresentationDetails.js
@@ -5,70 +5,70 @@ import {Button, Paper, TextField} from "@mui/material";
 
 export default function PresentationDetails(props) {
 
-  function goBackToMyPresentations() {
-    props.onPresentationDetails(ScreenIds.MY_PRESENTATIONS_SCREEN_ID);
-  }
+   function goBackToMyPresentations() {
+      props.onPresentationDetails(ScreenIds.MY_PRESENTATIONS_SCREEN_ID);
+   }
 
-  function goToTextTranscript() {
-    props.onPresentationDetails(ScreenIds.TRANSCRIPT_DETAILS_SCREEN_ID);
-  }
+   function goToTextTranscript() {
+      props.onPresentationDetails(ScreenIds.TRANSCRIPT_DETAILS_SCREEN_ID);
+   }
 
-  return(
-    <div>
-      <h2>" ENG 101 " Presentation </h2>
-      <p>Detailed Analysis Report</p>
-      <Grid container spacing={2}>
-      <Grid item xs={2}/>
-        <Grid item xs={4}>
-      <Card sx={{ minWidth: 275, maxWidth: 500, textAlign:"left", marginLeft: 5}}>
-        <p style={{textIndent: 40 , marginTop: 30}}>Final Grade is : " 86 "</p>
-        <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
-          <p style={{marginLeft: 10}}><u>Grades:</u></p>
-          <p style={{marginLeft: 10}}>Fluency : " 85 "</p>
-          <p style={{marginLeft: 10}}>Use of Language : " 86 "</p>
-          <p style={{marginLeft: 10}}>Body Language : " 87 "</p>
-        </Card>
+   return(
+      <div>
+         <h2>" ENG 101 " Presentation </h2>
+         <p>Detailed Analysis Report</p>
+         <Grid container spacing={2}>
+            <Grid item xs={2}/>
+            <Grid item xs={4}>
+               <Card sx={{ minWidth: 275, maxWidth: 500, textAlign:"left", marginLeft: 5}}>
+                  <p style={{textIndent: 40 , marginTop: 30}}>Final Grade is : " 86 "</p>
+                  <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
+                     <p style={{marginLeft: 10}}><u>Grades:</u></p>
+                     <p style={{marginLeft: 10}}>Fluency : " 85 "</p>
+                     <p style={{marginLeft: 10}}>Use of Language : " 86 "</p>
+                     <p style={{marginLeft: 10}}>Body Language : " 87 "</p>
+                  </Card>
 
-        <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
+                  <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
 
-          <p style={{marginLeft: 10}}>Some Remarks: <br/>
-            Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting,
-            remaining essentially unchanged. </p>
-        </Card>
-      </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card sx={{ minWidth: 275, maxWidth: 500, textAlign:"left", marginLeft: 5}}>
-            <p style={{textIndent: 40 , marginTop: 30}}>Duration : " 18.28 "</p>
-            <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
-              <p style={{marginLeft: 10}}><u>Speech Feedback:</u></p>
-              <p style={{marginLeft: 10}}>Percentage of Filler Words : " 19 % "</p>
-              <p style={{marginLeft: 10}}>Time Spent Idle : " 2.11 "</p>
-              <p style={{marginLeft: 10}}>Time Spent UMM ing : " 2.14</p>
-            </Card>
+                     <p style={{marginLeft: 10}}>Some Remarks: <br/>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the industry's
+                        standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a
+                        type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting,
+                        remaining essentially unchanged. </p>
+                  </Card>
+               </Card>
+            </Grid>
+            <Grid item xs={4}>
+               <Card sx={{ minWidth: 275, maxWidth: 500, textAlign:"left", marginLeft: 5}}>
+                  <p style={{textIndent: 40 , marginTop: 30}}>Duration : " 18.28 "</p>
+                  <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
+                     <p style={{marginLeft: 10}}><u>Speech Feedback:</u></p>
+                     <p style={{marginLeft: 10}}>Percentage of Filler Words : " 19 % "</p>
+                     <p style={{marginLeft: 10}}>Time Spent Idle : " 2.11 "</p>
+                     <p style={{marginLeft: 10}}>Time Spent UMM ing : " 2.14</p>
+                  </Card>
 
-            <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
+                  <Card sx={{ marginLeft: 5, marginRight: 5, marginBottom: 5}}>
 
-              <p style={{marginLeft: 10}}>Video Feedback: <br/>
-                When giving feedback you must always balance
-                what you want to say with what you feel will
-                be useful. Restrict your feedback to three
-                positive points and maybe one or two points
-                for development. There are only so many things
-                you can work on at the same time and it can
-                affect your motivation if you receive a list of
-                “negatives”.</p>
-            </Card>
-          </Card>
-        </Grid>
-      </Grid>
-      <br/><br/><Button variant="contained" onClick={goToTextTranscript}>See Text Transcript</Button><br/><br/>
-      <Button variant="contained" onClick={goBackToMyPresentations}>Back To My Presentations</Button><br/><br/><br/>
-    </div>
-  );
+                     <p style={{marginLeft: 10}}>Video Feedback: <br/>
+                        When giving feedback you must always balance
+                        what you want to say with what you feel will
+                        be useful. Restrict your feedback to three
+                        positive points and maybe one or two points
+                        for development. There are only so many things
+                        you can work on at the same time and it can
+                        affect your motivation if you receive a list of
+                        “negatives”.</p>
+                  </Card>
+               </Card>
+            </Grid>
+         </Grid>
+         <br/><br/><Button variant="contained" onClick={goToTextTranscript}>See Text Transcript</Button><br/><br/>
+         <Button variant="contained" onClick={goBackToMyPresentations}>Back To My Presentations</Button><br/><br/><br/>
+      </div>
+   );
 }
