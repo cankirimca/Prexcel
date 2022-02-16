af612a84afeec52d2fbd007f65b00419e167035e
Can Kırımca <can.kirimca@ug.bilkent.edu.tr>
2022-02-16 20:30:06 +0300

    partially implemented output formatting

--- project/backend/speech_to_text/SpeechToTextModel.py
+++ project/backend/speech_to_text/SpeechToTextModel.py
@@ -6,6 +6,7 @@ import wave
 import sys
 #sys.path.append(".")
 from VoiceDetectionManager import VoiceDetectionManager
+import time
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -16,6 +17,37 @@ beam_width = 1000
 lm_alpha = 0.93
 lm_beta = 1.18
 DEFAULT_SAMPLE_RATE = 16000
+
+def format_metadata_output(tokens):   
+    if not tokens:
+        return   
+    new_word = False
+    start_time = 0
+    end_time = 0
+    word = "" 
+    words = []
+    for token in tokens:
+        if new_word:
+            start_time = token.start_time
+            new_word = False
+        if token == tokens[len(tokens) - 1]:
+            end_time = token.start_time
+            word += token.text
+            words.append((word, len(word), end_time - start_time)) 
+            word = ""
+            break
+        if token.text != ' ':
+            end_time = token.start_time
+            word += token.text
+            new_word = True
+        else:
+            words.append((word, len(word), end_time - start_time))
+            word = ""    
+        #add information of spoken word, and its length and duration    
+        
+    print(words)  
+    f = open("output.txt","w+")     
+        
 
 class SpeechToTextModel:
 
@@ -51,18 +83,25 @@ class SpeechToTextModel:
             data16 = np.frombuffer(chunk, dtype = np.int16)
             self.stream.feedAudioContent(data16)
             print(self.stream.intermediateDecode())
-            start = end
+            start = end    
 
     def transcribe_live(self, buffer, stop_flag):
+        ttime = time.time()
         frames = self.vdm.vad_collector(stop_flag)
         for frame in frames:
             if frame is not None:
                 # if spinner:
                 #     spinner.start()
                 self.stream.feedAudioContent(np.frombuffer(frame, np.int16))
-                #print(self.stream.intermediateDecode())
-                buffer[0] = (self.stream.intermediateDecodeWithMetadata())
-                print(buffer[0], type)      
+                x = (self.stream.intermediateDecodeWithMetadata())
+                tokens = x.transcripts[0].tokens
+                #<class 'deepspeech.impl.Metadata'>
+            if time.time() - ttime > 5:
+                format_metadata_output(tokens)
+                break
+
+    
+                      
 
 stm = SpeechToTextModel()
 buffer = [""]
