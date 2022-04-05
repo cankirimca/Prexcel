0fecdda2efbb8cacda88550a2aff7f131ca9c118
BarisTiftik <brstftk@gmail.com>
2022-04-05 22:22:59 +0300

    Props logic added ChatBot.js added

--- project/frontend/prexcel/src/pages/MyPresentations.js
+++ project/frontend/prexcel/src/pages/MyPresentations.js
@@ -34,26 +34,60 @@ const columns = [
 ];
 
 // todo to be removed once connected to the database
-/*const rows = [
-  { id: 1, name: "ENG 101", grade: 'B-', user_id: "12.00"},
-  { id: 2, name: "ENG 102", grade: 'A-', user_id: "12.00"},
-  { id: 3, name: "CS 102", grade: 'B', user_id: "12.00"},
-  { id: 4, name: "CS 411", grade: 'S', user_id: "12.00"},
-  { id: 5, name: "MSC 110", grade: 'S', user_id: "12.00"},
-];*/
+const rows = [
+   { id: 1, name: "ENG 101", transcript: "ENG 101 Lesson -33\n" +
+         "      2 Lesson – 33 (Essay Writing)\n" +
+         "      In the past few lessons we have looked at paragraph writing. Now we shall move forward and look at longer pieces of writing which consist of several paragraphs. The problems we considered in sentence and paragraph writing are also the fundamental problems of longer composition – the same problems of unity, coherence, and emphasis. While some topics can be treated in a single paragraph others require more elaborate development. In longer compositions we\n" +
+         "\n" +
+         "      3 find problems of arrangement\n" +
+         "      find problems of arrangement. A longer composition, such as an essay, divides itself usually into a number of parts. In what order shall we present them? How much emphasis should be given to each fact? These are some of the problems faced in essay writing.\n" +
+         "      Now a lot depends on the purpose of your writing. If you are writing primarily to give information, then you need to be as exact as possible in the interest of clear, logical presentation of facts. If you are writing to describe something, or to create an image or a\n" +
+         "\n" +
+         "      4 picture then you shall perhaps put less emphasis upon accurate measurement and more upon suggestive and revealing details. If you are writing to tell a story, you will have to decide upon the point of highest interest and arrange your material carefully to give your reader the feeling or illusion of taking part in the action.\n" +
+         "      Writing an essay is not more difficult than writing a paragraph. There is only one difference – of length. The principles of\n" +
+         "\n" +
+         "      5 organization are the same for both: so if you can write a good paragraph, you can also write a good essay.\n" +
+         "      Process of Writing\n" +
+         "      Writing usually takes place in steps or stages. There are five stages or steps in the writing process. (1) Getting ideas: brainstorming, clustering & free writing (2) making brief outline (3) writing the 1st draft (4) revising (5) proof reading\n" +
+         "\n" +
+         "      6 .\n" +
+         "      Process of Writing: Five stages / steps\n" +
+         "      Getting ideas: brainstorming, clustering, free writing\n" +
+         "      making brief outline\n" +
+         "      writing the 1st draft\n" +
+         "      revising\n" +
+         "      proof reading\n" +
+         "\n" +
+         "      7 Step I: Getting Ideas: Primarily from reading, talking to people, listening to talk shows, TV programs on current issues etc.\n" +
+         "      (a) Brainstorming – For ideas\n" +
+         "      jot down points or ideas and their details as they come to your mind. Just write them down without putting them in any special order. Try to accumulate as many details as you can think of. This is one strategy of beginning an essay. The other strategy is\n" +
+         "\n" +
+         "      8 (b) Clustering: here you begin by writing your subject or topic in the center of a blank sheet of paper. Then as ideas come into your mind you put them down in boxes or circles around the subject or topic. You will now see a sample of clustering.\n" +
+         "      (c) Free writing, this is also a very useful technique. Here you just write down whatever comes into your mind about the topic. Continue writing for sometime without stopping to worry about grammar or spellings etc.", grade: 'B-', user_id: "12"},
+   { id: 2, name: "ENG 102", transcript: "Transcript eng \n detailssssssss" , grade: 'A-', user_id: "12"},
+   { id: 3, name: "CS 102", transcript: "Transcript cs102 \n detailsssssssss" , grade: 'B', user_id: "12"},
+   { id: 4, name: "CS 411", transcript: "Transcript cs411  \n detailssssssss" , grade: 'S', user_id: "12"},
+   { id: 5, name: "MSC 110", transcript: "Transcript msc110 \n detailssssssss" , grade: 'S', user_id: "12"},
+   { id: 6, name: "GE 301", transcript: "Transcript ge301 \n detailssssssss" , grade: 'S-', user_id: "12"},
+];
 
 
 export default function MyPresentations(props) {
 
    useEffect(fetchPresentations, []);
 
-   const [rows, setRows] = useState([]);
+   /* IMPORTANT !!
+   THIS HOOK TO BE USED FOR ACTUAL DB CONNECTION
+    */
+   //const [rows, setRows] = useState([]);
 
    function goToPresentationDetails() {
       props.onMyPresentationsHandler(ScreenIds.PRESENTATION_DETAILS_SCREEN_ID);
    }
 
-   const [selectionModel, setSelectionModel] = useState(() => rows.filter((r) => true).map((r) => r.presentation_id));
+   /* DO NOT OPEN BELOW COMMENT */
+   //const [selectionModel, setSelectionModel] = useState(() => rows.filter((r) => true).map((r) => r.presentation_id));
+   const [selectedRows, setSelectedRows] = useState([]);
 
    function goBackToMainMenu() {
       props.onMyPresentationsHandler(ScreenIds.MAIN_MENU_SCREEN_ID);
@@ -65,7 +99,7 @@ export default function MyPresentations(props) {
 
    const getPresentations = (userID) => {
       let success = true;
-      fetch('http://localhost:5000/getPresentations', {
+      /*fetch('http://localhost:5000/getPresentations', {
          method: 'POST',
          headers: {
             'Content-Type':'application/json'
@@ -80,7 +114,7 @@ export default function MyPresentations(props) {
             setRows(fixData(data));
             console.log(rows);
          })
-         .catch(error => console.log(error))
+         .catch(error => console.log(error))*/
 
       return success;
    };
@@ -106,25 +140,41 @@ export default function MyPresentations(props) {
 
    return (
       <div>
-         <div>
-            {fetchPresentations}
-         </div>
-         <div style={{ height: 400, width: '100%' }}>
-            <p>My Presentations</p>
-            <DataGrid
-               rows={rows}
-               columns={columns}
-               pageSize={5}
-               rowsPerPageOptions={[5]}
-               checkboxSelection
-               disableSelectionOnClick
+         {fetchPresentations}
+         <Grid item xs={12}>
+            <h1>My Presentations</h1>
+         </Grid>
+         <Grid container spacing={2}>
+            <Grid item xs={3}/>
+            <Grid item xs={6}>
+               <Paper sx={{height:400, marginTop: '20%', marginBottom:'5%', flexDirection:'row', alignItems:'center', justifyContent:'center'}} elevation={3}>
+                  <DataGrid
+                     rows={rows}
+                     columns={columns}
+                     pageSize={5}
+                     rowsPerPageOptions={[5]}
+                     checkboxSelection
+                     disableSelectionOnClick
 
-               selectionModel={selectionModel}
-               onSelectionModelChange={setSelectionModel}
-            />
+                     //selectionModel={selectionModel}
+                     onSelectionModelChange={(ids) => {
+                        const selectedIDs = new Set(ids);
+                        const selectedRows = rows.filter((row) =>
+                           selectedIDs.has(row.id),
+                        );
 
-            <br/><Button variant="contained" onClick={goToPresentationDetails}>Presentation Details</Button><br/><br/>
-            <Button variant="contained" onClick={goBackToMainMenu}>Back To Main Menu</Button>
-         </div></div>
+                        setSelectedRows(selectedRows);}}
+                  />
+                  {console.log(JSON.stringify(selectedRows, null, 4))}
+                  <br/>
+                  <Button disabled={!(selectedRows.length === 1)} variant="contained" onClick={goToPresentationDetails}>View Report</Button><br/><br/>
+                  <Button disabled={!(selectedRows.length >= 2)} variant="contained">See Progress</Button><br/><br/>
+                  <Button variant="contained" onClick={goBackToMainMenu}>Back To Main Menu</Button>
+               </Paper>
+            </Grid>
+            <Grid item xs={3}/>
+         </Grid>
+         {props.onPresentationSelection(selectedRows)}
+      </div>
    );
 }
--- project/frontend/prexcel/src/pages/TranscriptDetails.js
+++ project/frontend/prexcel/src/pages/TranscriptDetails.js
@@ -7,25 +7,31 @@ export default function TranscriptDetails(props) {
   function goBackToPresentationDetails() {
     props.onTranscriptDetails(ScreenIds.PRESENTATION_DETAILS_SCREEN_ID);
   }
-  // to be removed
+
   return(
+     <div>
+       <h2>Transcript {props.selectedPresentations[0].name} Presentation</h2>
+       <p style={{textAlign: "left", marginLeft: 40 , marginTop: 30, marginBottom: 30, marginRight: 40}}>
+         {props.selectedPresentations[0].transcript}
+       </p>
+       <Button variant="contained" onClick={goBackToPresentationDetails}>Back To Presentation Details</Button>
+     </div>
+  );
+
+  /*return(
     <div>
     <h2>Transcript " ENG 101 " Presentation</h2>
     <p style={{textAlign: "left", marginLeft: 40 , marginTop: 30, marginBottom: 30, marginRight: 40}}> ENG 101 Lesson -33
       2 Lesson – 33 (Essay Writing)
       In the past few lessons we have looked at paragraph writing. Now we shall move forward and look at longer pieces of writing which consist of several paragraphs. The problems we considered in sentence and paragraph writing are also the fundamental problems of longer composition – the same problems of unity, coherence, and emphasis. While some topics can be treated in a single paragraph others require more elaborate development. In longer compositions we
-
       3 find problems of arrangement
       find problems of arrangement. A longer composition, such as an essay, divides itself usually into a number of parts. In what order shall we present them? How much emphasis should be given to each fact? These are some of the problems faced in essay writing.
       Now a lot depends on the purpose of your writing. If you are writing primarily to give information, then you need to be as exact as possible in the interest of clear, logical presentation of facts. If you are writing to describe something, or to create an image or a
-
       4 picture then you shall perhaps put less emphasis upon accurate measurement and more upon suggestive and revealing details. If you are writing to tell a story, you will have to decide upon the point of highest interest and arrange your material carefully to give your reader the feeling or illusion of taking part in the action.
       Writing an essay is not more difficult than writing a paragraph. There is only one difference – of length. The principles of
-
       5 organization are the same for both: so if you can write a good paragraph, you can also write a good essay.
       Process of Writing
       Writing usually takes place in steps or stages. There are five stages or steps in the writing process. (1) Getting ideas: brainstorming, clustering & free writing (2) making brief outline (3) writing the 1st draft (4) revising (5) proof reading
-
       6 .
       Process of Writing: Five stages / steps
       Getting ideas: brainstorming, clustering, free writing
@@ -33,15 +39,13 @@ export default function TranscriptDetails(props) {
       writing the 1st draft
       revising
       proof reading
-
       7 Step I: Getting Ideas: Primarily from reading, talking to people, listening to talk shows, TV programs on current issues etc.
       (a) Brainstorming – For ideas
       jot down points or ideas and their details as they come to your mind. Just write them down without putting them in any special order. Try to accumulate as many details as you can think of. This is one strategy of beginning an essay. The other strategy is
-
       8 (b) Clustering: here you begin by writing your subject or topic in the center of a blank sheet of paper. Then as ideas come into your mind you put them down in boxes or circles around the subject or topic. You will now see a sample of clustering.
       (c) Free writing, this is also a very useful technique. Here you just write down whatever comes into your mind about the topic. Continue writing for sometime without stopping to worry about grammar or spellings etc.
     </p>
       <Button variant="contained" onClick={goBackToPresentationDetails}>Back To Presentation Details</Button>
     </div>
-  );
+  );*/
 }
