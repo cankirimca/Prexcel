0d5b53100108d55802c4f2c9e98c596ac71bdeb4
CanKirsallioba <cankirsallioba@gmail.com>
2022-04-11 22:47:29 +0300

    added file converter to PresentationAnalyzer.py

--- project/backend/PresentationAnalyzer.py
+++ project/backend/PresentationAnalyzer.py
@@ -3,6 +3,9 @@ from project.backend.database.UserDataManager import UserDataManager
 from project.backend.face_detection.FaceDetection import FaceDetection
 from project.backend.speech_analysis.SpeechAnalyzer import SpeechAnalyzer
 
+import os
+import sys
+from moviepy.editor import VideoFileClip
 
 class PresentationAnalyzer:
 
@@ -31,97 +34,11 @@ class PresentationAnalyzer:
     def process_recording(self):
         self.stt.transcribe_stream(self.file_path)
         transcript, word_count, duration, wpm, gap_ratio, filler_ratio = self.sa.analyzed_tokens(self.tokens)
-        self.udm.add_presentation(self.presentation_name, transcript, self.user_id, wpm, duration, gap_ratio, filler_ratio, word_count) 
+        self.udm.add_presentation(self.presentation_name, transcript, self.user_id, wpm, duration, gap_ratio, filler_ratio, word_count)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def convert_video_to_audio(self, output_extension):
+        file_name, extension = os.path.splitext(self.file_path)
+        clip = VideoFileClip(self.file_path)
+        clip.audio.write_audiofile(f"{file_name}.{output_extension}")
 
