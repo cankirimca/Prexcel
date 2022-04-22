d1ea08b46acaff364aa015b66d8dec366413fc6e
Burak Yiğit Uslu <burakyuslu@gmail.com>
2022-04-22 23:03:46 +0300

    Transcript parsing is now working with @CanKirsallioba @cankirimca

--- project/frontend/prexcel/src/pages/TranscriptDetails.js
+++ project/frontend/prexcel/src/pages/TranscriptDetails.js
@@ -22,12 +22,59 @@ export default function TranscriptDetails(props) {
 
    // todo
   function processTranscript() {
-     let transcript = props.selectedPresentations[0].transcript;
+     // let transcript = props.selectedPresentations[0].transcript;
 
-     transcriptTokens = transcript.split(" ");
+     let transcript = "The short story is {dragged} usually concerned with a single {dragged/} effect conveyed in only one or {repeated} a few significant episodes {repeated/} or scenes. The form {filler} encourages {filler/} economy of setting, {dragged} concise {repeated} narrative, and the {repeated/} omission of a {dragged/} complex plot; character is disclosed {/space/} in action and dramatic encounter but is seldom fully developed. Despite {/space/} its relatively limited scope, though, a short story is often judged by its ability {dragged} to provide {/space/} a “complete” or {dragged/} satisfying treatment of its characters and subject.";
+
+     let transcriptTokens = transcript.split(" ");
+
+     let processTranscriptArrTemp = [];
+     let tagStack = [];
+
+     tagStack.push(str_type_regular);
+
      for (let i = 0; i < transcriptTokens.length; i++) {
-         processTranscriptArrTemp.push( { type: tagStack[tagStack.length-1], word: transcript[i]} );
-     } 
+        // console.log("i:" + transcriptTokens[i]);
+        let token = transcriptTokens[i];
+        if( token !== ""){
+           if(token === "{dragged}"){
+              tagStack.push(str_type_dragged);
+           }
+           else if (token === "{dragged/}"){
+              tagStack.pop();
+           }
+           else if(token === "{filler}"){
+              tagStack.push(str_type_filler);
+           }
+           else if (token === "{filler/}"){
+              tagStack.pop();
+           }
+           else if(token === "{repeated}"){
+              tagStack.push(str_type_repeated);
+           }
+           else if (token === "{repeated/}"){
+              tagStack.pop();
+           }
+           // todo check
+           else if (token === "{/space/}"){
+              transcriptTokens[i] = "(...)";
+              console.log("BRUH WOHOOO");
+           }
+           console.log("TRANSCRIPT TOKEN:" + transcriptTokens[i]);
+
+           // push into the array
+           if( transcriptTokens[i] === "(...)" || transcriptTokens[i][0] !== "{"){
+              if( transcriptTokens[i] !== "(...)"){
+                 processTranscriptArrTemp.push( { type: tagStack[tagStack.length-1], word: transcriptTokens[i]} );
+              }
+              else {
+                 processTranscriptArrTemp.push( { type: str_type_gap, word: transcriptTokens[i]} );
+              }
+           }
+        }
+     }
+
+     console.log(processTranscriptArrTemp);
 
      /*
      let indexTranscript = 0;
