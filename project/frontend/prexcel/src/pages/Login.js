0c1187c7b3ca51c213c0886994f2d2a8ab41f09c
BarisTiftik <brstftk@gmail.com>
2022-05-03 15:14:34 +0300

    login error w/burakyuslu

--- project/frontend/prexcel/src/pages/Login.js
+++ project/frontend/prexcel/src/pages/Login.js
@@ -5,8 +5,15 @@ import ScreenIds from "./ScreenIds";
 
 // material ui components
 import {Button, Grid, Paper, TextField} from "@mui/material";
+import DialogBox from "./DialogBox";
 
 const Login = (props) => {
+
+   const [dialogOpen, setDialogOpen] = useState(false);
+   const handleClose = () => {
+      setDialogOpen(false);
+   };
+
    const [count, setCount] = useState(0);
    useEffect(() => {
       // if successful change to main menu
@@ -18,7 +25,6 @@ const Login = (props) => {
       }
       // otherwise reshow the login screen
       else {
-         // todo display login failed message
          props.onLoginHandler(ScreenIds.LOGIN_SCREEN_ID);
       }
    });
@@ -41,6 +47,10 @@ const Login = (props) => {
             .then((data) => {
                console.log(data);
                setCount(data);
+               if (data === -1) {
+                  console.log("dialog box opening");
+                  setDialogOpen(true);
+               }
             })
             .catch(error => console.log(error))
 
@@ -102,6 +112,9 @@ const Login = (props) => {
                <Grid style={{marginTop: '5%', paddingBottom: '5%'}} item xs={12}>
                   <Button style={{backgroundColor:'#507786', marginRight: '5%'}} variant="contained" onClick={AttemptLogin}>Log-In</Button>
                   <Button style={{backgroundColor:'#507786'}} variant="contained" onClick={goToSignUp}>Sign-Up</Button>
+                  <DialogBox open={dialogOpen} onClose={handleClose}
+                             dialogContent={"Please re-enter your login credentials."}
+                             dialogTitle={"Incorrect password or username!"}/>
                </Grid>
 
             </Paper>
